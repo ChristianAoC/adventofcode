0dca7be9e9a464b68fcd8932923b6a6166ee0f88
ChristianAoC <christianaoc@hcac.de>
2024-12-03 06:22:45 +0000

    AoC 2024 day 03

---
+++ 2024/03/solver.py
@@ -0,0 +1,25 @@
+import re
+""" Advent of Code solver class """
+with open('input.txt', 'r', encoding='utf8') as file_handle:
+    inp = "".join([line.strip() for line in file_handle])
+
+def task1():
+    """ Task 1 solver """
+    result = 0
+    res = re.findall("mul\((\d+),(\d+)\)", inp)
+    for a, b in res:
+        result += int(a) * int(b)
+    return result
+
+print("Task 1:", task1())
+
+def task2():
+    """ Task 2 solver """
+    inp3 = re.sub("don't\(\).*?(?:$|do\(\))", "", inp)
+    result = 0
+    res = re.findall("mul\((\d+),(\d+)\)", inp3)
+    for a, b in res:
+        result += int(a) * int(b)
+    return result
+
+print("Task 2:", task2())
